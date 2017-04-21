8b4ec4f944a5f5cd0cde27e8808aaad861a87a11
XuGuo <xuguo2002@gmail.com>
2017-04-21 01:00:38 -0700

    finished main App component

--- project/p2_mygithub/src/components/App.jsx
+++ project/p2_mygithub/src/components/App.jsx
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			username : 'xuguo',
+			userData : [],
+			userRepos : [],
+			perPage : 5
+		}
+
+	}
+
 	render(){
 		return(
 			<div>
-				My App
+				{this.state.username}
 			</div>
 		)
 	}
+}
+
+App.propTypes = {
+	clientId : PropTypes.string,
+	clientSerect : PropTypes.string
+};
+App.defanltProps = {
+	clientId : '6f4d328a799fb877b61f',
+	clientSerect : '9651f6ea6848d2f2828aaec5031be391437ef54d'
 }
 
 export default App
